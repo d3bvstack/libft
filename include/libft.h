e7e366c5a211c6503a62aada71958d278d683d2c
d3bvstack <180553755+d3bvstack@users.noreply.github.com>
2025-04-13 12:06:14 +0200

    libft.h: Refactored and added definitions

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:18:14 by dbarba-v          #+#    #+#             */
-/*   Updated: 2025/04/12 18:16:17 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/04/13 09:57:41 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,61 @@
 # include <stdarg.h>
 # include <limits.h>
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
-void	ft_free_all(void *first, ...);
-void	ft_free_array(char **array);
-char	*get_next_line(int fd);
-void	ft_free(char **old_mem, char *new_mem);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putstr_fd(char *s, int fd);
-char	**ft_split(char *s, char c);
+// Character functions
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isspace(int c);
+int		ft_issign(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+// String functions
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t dst_memsize);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *str, int c);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *source, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strjoin_three(char *s1, char *s2, char *s3);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_substr(char *source, unsigned int start, size_t n);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+// Memory functions
+void	*ft_memset(void *dest, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+// Conversion functions
+int		ft_atoi(const char *nptr);
+long	ft_atol(const char *nptr);
+char	*ft_itoa(int n);
+
+// File descriptor functions
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+// Memory management functions
+void	ft_free(char **old_mem, char *new_mem);
+void	ft_free_array(void **array);
+void	ft_free_array2d(void ***array);
+void	ft_free_all(void *first, ...);
+
+// File I/O functions
 char	*get_next_line(int fd);
 
 #endif
