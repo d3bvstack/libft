e847b438fe36e60a94840e51a0273b716c73b6c3
d3bvstack <180553755+d3bvstack@users.noreply.github.com>
2025-04-13 16:31:39 +0200

    libft.h: Update function declarations and add linked list structure and functions

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:18:14 by dbarba-v          #+#    #+#             */
-/*   Updated: 2025/04/13 09:57:41 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/04/13 16:22:38 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,5 +84,23 @@ void	ft_free_all(void *first, ...);
 
 // File I/O functions
 char	*get_next_line(int fd);
+
+// Simple list structure
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+// Lists and nodes functions
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
